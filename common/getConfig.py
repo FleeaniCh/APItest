27f37bd633baaa1cf6e54538c2e71c10186a6acb
FleeaniCh <17354108830@163.com>
2021-03-02 17:51:37 +0800

    first commit V1.0.1 2021-03-02

---
+++ common/getConfig.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Author: Renkai
+
+import os
+
+from common.getConfig import *
+
---
+++ common/initPath.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Author: Renkai
+"""
+    获取各模块路径
+"""
+import os
+
+BASEDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# print(BASEDIR)  # D:\self-Study\code\APItest
+COMMONDIR = os.path.join(BASEDIR, 'common')
+# print(COMMONDIR)
+CONFDIR = os.path.join(BASEDIR, 'config')
+DATADIR = os.path.join(BASEDIR, 'data')
+LIBDIR = os.path.join(BASEDIR, 'library')
+LOGDIR = os.path.join(BASEDIR, 'log')
+REPORTDIR = os.path.join(BASEDIR, 'report')
+CASEDIR = os.path.join(BASEDIR, 'testcase')
+print(CASEDIR)
