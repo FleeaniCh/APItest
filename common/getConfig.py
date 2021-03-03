419256e544ec3648276c8792850f9ba129e831d5
FleeaniCh <17354108830@163.com>
2021-03-03 17:41:15 +0800

    V1.0.1 2021-03-03

--- common/getConfig.py
+++ common/getConfig.py
@@ -3,7 +3,49 @@
 
 # Author: Renkai
 
-import os
+from common.initPath import *
+from configparser import ConfigParser
 
-from common.getConfig import *
 
+class Config(ConfigParser):
+    def __init__(self):
+        super().__init__()
+        self.conf_file = os.path.join(CONFDIR, 'baseCon.ini')
+        super().read(self.conf_file, encoding='utf-8')  # 读取配置文件
+
+    def getAllsection(self):
+        '''返回配置文件的所有节点名称'''
+        return super().sections()
+
+    def getOptions(self, sectionName):
+        '''返回指定节点的key'''
+        return super().options(sectionName)
+
+    def getItems(self, sectionName):
+        '''返回指定节点的key-value'''
+        return super().items(sectionName)
+
+    def getValue(self, sectionName, key):
+        '''返回指定节点指定key的值'''
+        return super().get(sectionName, key)
+
+    def saveData(self,sectionName,key,value):
+        """
+            添加配置
+        :param sectionName:节点名称section
+        :param key:option
+        :param value:
+        :return:
+        """
+        super().set(section=sectionName,option=key,value=value)
+        super().write(open(self.conf_file,'w'))
+
+
+if __name__ == '__main__':
+    conf = Config()
+    # print(conf.getAllsection())
+    # print(conf.getOptions('db'))
+    # print(conf.getItems('db'))
+    # print(conf.get('test_data', 'pwd'))
+    conf.saveData('db','newKey','newVlue')
+    print(conf.getItems('db'))
--- common/initPath.py
+++ common/initPath.py
@@ -8,13 +8,12 @@
 import os
 
 BASEDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# print(BASEDIR)  # D:\self-Study\code\APItest
+# print(BASEDIR)  # D:\Git\APItest
 COMMONDIR = os.path.join(BASEDIR, 'common')
-# print(COMMONDIR)
 CONFDIR = os.path.join(BASEDIR, 'config')
 DATADIR = os.path.join(BASEDIR, 'data')
 LIBDIR = os.path.join(BASEDIR, 'library')
 LOGDIR = os.path.join(BASEDIR, 'log')
 REPORTDIR = os.path.join(BASEDIR, 'report')
 CASEDIR = os.path.join(BASEDIR, 'testcase')
-print(CASEDIR)
+# print(CASEDIR)
